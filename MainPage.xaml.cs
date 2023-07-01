bd625dc1c0912a2fef4624d68514103d00afcda2
7JP10 <122068042+7JP10@users.noreply.github.com>
2023-07-01 02:53:02 -0400

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace PracticePanther.MAUI;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void ClientsClicked(object sender, EventArgs e)
+    {
+        Shell.Current.GoToAsync("//Client");
+    }
+
+    public void EmployeeClicked(object sender, EventArgs e)
+    {
+        Shell.Current.GoToAsync("//ManageEmployee");
+    }
+}
+
+
